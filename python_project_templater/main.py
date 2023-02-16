8373306555066d25fefa24fc533dbf42019ff413
zeph1rr <grianton535@gmail.com>
2023-02-16 17:46:55 +0300

    feat(actions) added github actions

--- python_project_templater/main.py
+++ python_project_templater/main.py
@@ -1,14 +1,27 @@
+"""
+Module for creating projects based on poetry from predefined templates
+"""
 import argparse
 import os
 import pathlib
 import shutil
 
-from typing import Optional
-
 from loguru import logger
 
 
-def create_project(template: str, project_path: str) -> Optional[bool]:
+def create_project(template: str, project_path: str) -> bool:
+    """
+    Creating project from template
+
+    :param template: Name of chosen template
+    :type template: str
+
+    :param project_path: Path to new project
+    :type project_path: str
+
+    :returns: status of creating
+    :rtype: bool
+    """
     templates = os.listdir(os.path.join(pathlib.Path(__file__).parent.resolve(), "templates"))
     if template not in templates:
         raise ValueError(f"Template not found! ({template})")
@@ -17,13 +30,14 @@ def create_project(template: str, project_path: str) -> Optional[bool]:
         shutil.copytree(template_path, project_path)
         os.chdir(project_path)
         os.system("git init")
-    except Exception as _ex:
+    except FileExistsError as _ex:
         logger.error(_ex)
         return False
     return True
 
 
 def main():
+    """Main function of module"""
     parser = argparse.ArgumentParser(description="create new python project from template")
     parser.add_argument("-t", '--template', required=True, help='template for project')
     parser.add_argument("-p", '--project', required=True, help="project path")
